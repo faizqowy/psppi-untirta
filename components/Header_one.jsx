4ed9a81ba4180010f13681b6b515fffb9b80b728
rizkingrh <rizkingrh.mhd@gmail.com>
2024-02-01 21:48:57 +0700

    add content header one

--- components/Header_one.jsx
+++ components/Header_one.jsx
@@ -1,7 +1,20 @@
+import { FaSearch } from 'react-icons/fa';
+
 export default function Header_one() {
     return (
-        <header>
-            <div>Header 1</div>
-        </header>
+        <div className="bg-black shadow py-3">
+            <div className="flex justify-center">
+                <div className="w-[85%]">
+                    <div className="text-gray-300 text-xs flex justify-end">
+                        <a href="https://untirta.ac.id" className="hover:text-white">
+                            UNTIRTA.ac.id
+                        </a>
+                        <span className="pt-0.5 ml-4 cursor-pointer hover:text-white">
+                            <FaSearch />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }

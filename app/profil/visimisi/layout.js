f013244d816a90a1b0776bcf3893d707e1d0941e
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2024-02-03 10:15:00 +0700

    font

--- app/profil/visimisi/layout.js
+++ app/profil/visimisi/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter, Nunito_Sans } from "next/font/google";
 import "../../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",

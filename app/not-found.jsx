6a70134d6228d35e56fde519c55c6f6c4c18c78e
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2024-02-04 21:16:42 +0700

    revamp

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -12,14 +12,6 @@ export default function NotFoundPage() {
         <div className="w-[78%] flex flex-col justify-center">
           <Header_two />
           <section className="p-8 bg-white text-gray-600">
-            <div className="flex mb-7">
-              <a href="/" className="hover:cursor-pointer">
-                <IoHomeSharp className="inline-block text-sm hover:fill-blue-950 duration-300" />
-              </a>
-              <IoIosArrowForward className="mt-2 inline-block text-sm" />
-              <h1 className="text-sm font-bold ml-1 mt-1">Jadwal Kegiatan</h1>
-            </div>
-            <h1 className="text-3xl font-bold mb-4">Jadwal Kegiatan</h1>
             <p className="text-sm mb-4 py-40">
               Halaman yang Anda cari tidak ditemukan. Coba cari lagi atau kembali ke halaman utama.
             </p>

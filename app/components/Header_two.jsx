352c781895928dd97e87c887115c340101ef6226
nieehhh <kemalmohht@gmail.com>
2024-02-05 23:50:48 +0700

    header2

--- app/components/Header_two.jsx
+++ app/components/Header_two.jsx
@@ -1,8 +1,51 @@
+import { FaSearch } from "react-icons/fa";
+import "./Headerone.css";
+import Image from "next/image";
+
 export default function Header_two() {
   return (
-    <header>
-      <div>Header 2</div>
-      
-    </header>
+    <div className="bg-transparent shadow py-3">
+      <div className="flex justify-center">
+        <div className="lg:w-[100%] w-[100%] lg:mr-0 mr-4 flex flex-col lg:flex-row">
+          <div 
+            style={{
+              fontSize: "20px",
+              fontWeight: "bold",
+              textAlign: "left", // Mengubah properti textAlign menjadi "left"
+              margin: "auto", // Menambah properti margin
+            }}
+          >
+            <p>PROGRAM STUDI</p>
+            <p>PROGRAM PROFESI INSINYUR</p>
+            <p>UNIVERSITAS SULTAN AGENG TIRTAYASA</p>
+          </div>
+          <div className="flex justify-end">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Image
+                src="/images/untirtalogo.jpg"
+                alt="Unitrta Logo"
+                className="object-cover w-[50%] h-[100px] object-right"
+                width={1920}
+                height={1080}
+              />
+              <Image
+                src="/images/kemendikbud.jpg"
+                alt="Kemendikbud"
+                className="object-cover w-[50%] h-[100px] object-right"
+                width={1920}
+                height={1080}
+              />
+              <Image
+                src="/images/pii.jpg"
+                alt="PII Logo"
+                className="object-cover w-[50%] h-[100px] object-right"
+                width={1920}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }

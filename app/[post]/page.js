da04af4c0f39a2a83aa54e27d48e33c98b141a74
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2024-02-07 16:29:01 +0700

    header 2 revamp

--- app/[post]/page.js
+++ app/[post]/page.js
@@ -7,6 +7,10 @@ import Header_one from "../components/Header_one";
 import Header_two from "../components/Header_two";
 import Footer from "../components/Footer";
 
+import { FaFolderOpen } from "react-icons/fa6";
+import { FaClock } from "react-icons/fa6";
+import { IoIosPricetag } from "react-icons/io";
+
 import { IoHomeSharp } from "react-icons/io5";
 import { IoIosArrowForward } from "react-icons/io";
 import { FaArrowRight } from "react-icons/fa";
@@ -19,8 +23,10 @@ export default function Post() {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const val = searchParams.get("postId")
+  var search = searchParams.get("search");
   const router = useRouter();
   
+  search = search === null ? null : search;
   useEffect(() => {
     fetch(`/local-data/data.json`)
       .then((res) => res.json())
@@ -32,7 +38,7 @@ export default function Post() {
 
   if(post?.data.length < val) {
     router.push("/post?postId=1");
-  }else{
+  }else if(search === null){
   return (
     <main className="flex flex-col justify-center w-[100%]">
       <Header_one />
@@ -117,5 +123,76 @@ export default function Post() {
       </div>
     </main>
   );
-}
+  }else{
+    return(
+    <main className="flex flex-col justify-center w-[100%]">
+      <Header_one />
+      <div className="flex flex-row justify-center">
+        <div className="lg:w-[78%] w-[100%] flex flex-col justify-center">
+          <Header_two />
+          <section className="p-8 bg-white text-gray-600">
+            <div className="flex mb-7">
+              <a href="/" className="hover:cursor-pointer">
+                <IoHomeSharp className="inline-block text-sm hover:fill-blue-950 duration-300" />
+              </a>
+              <IoIosArrowForward className="mt-2 inline-block text-sm" />
+              <h1 className="text-sm font-bold ml-1 mt-1">SEARCH RESULT</h1>
+            </div>
+            <h2>SEARCH RESULT</h2>
+            <div className="grid md:grid-cols-2 grid-cols-1 mb-14">
+              {post?.data.map((item, index) => {
+                if(item.title.toLowerCase().includes(search.toLowerCase())){
+                  return (
+                    <div key={index} className="p-5">
+                      <h2 className="text-xl font-bold border-b border-black mb-2">{item.title}</h2>
+                      <Image src={item.img} className="object-cover w-[100%] h-[250px] border-4 mb-3" width={1920} height={1080} />
+                      <p className="mb-5 text-sm">
+                        {item.content.slice(0, 200) + "..."}
+                      </p>
+                      <a href={"/post?postId=" + (index+1)} className="bg-gray-300 p-2 border-b border-black rounded-md hover:bg-gray-400 active:bg-gray-500 active:text-white duration-200">
+                        Continue Reading
+                      </a>
+                      <div className="mt-5">
+                        <div className="flex">
+                          <FaClock className="mt-[2px] mr-2" />
+                          <p className="text-sm mr-7">{item.date}</p>
+                          <FaFolderOpen className="mt-[2px] mr-2" />
+                          <p className="text-sm">
+                            <a href="#" className="text-blue-950">
+                              {item.category}
+                            </a>
+                          </p>
+                        </div>
+                        <div className="flex mt-3">
+                          <IoIosPricetag className="mt-[2px] mr-1" size={30} />
+                          <p className="text-sm mr-7">
+                            {item.tags.map((tag, index) => {
+                              if(index === item.tags.length-1){
+                                return (
+                                  <a key={index} href="#" className="text-blue-800 hover:text-blue-950">
+                                    {tag}
+                                  </a>
+                                )
+                              }else{
+                              return (
+                                <a key={index} href="#" className="text-blue-800 hover:text-blue-950">
+                                  {tag},
+                                </a>
+                              )}
+                            })}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+          </section>
+          <Footer />
+        </div>
+      </div>
+    </main>)
+  }
+
 }

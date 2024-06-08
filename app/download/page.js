6017c9feb5a57d703a75a8423e16dab4d702554d
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2024-06-08 17:34:59 +0700

    File download

--- app/download/page.js
+++ app/download/page.js
@@ -7,6 +7,8 @@ import { Montserrat } from "next/font/google";
 
 import { FaHome } from "react-icons/fa";
 import { IoIosArrowForward } from "react-icons/io";
+import { FaDownload } from "react-icons/fa6";
+
 
 import Image from "next/image";
 
@@ -33,51 +35,17 @@ export default function Home() {
               <p className="font-bold">Download</p>
             </div>
 
-            <h1 className={"text-3xl font-bold mb-4 " + montserrat.className}>
+            <h1 className={"text-3xl font-bold mb-12 " + montserrat.className}>
               Download
             </h1>
 
             <div className="flex flex-col gap-2">
-              <a href="#">
-                <Image
-                  src="/images/download/profil.jpg"
-                  alt="profil"
-                  width={235}
-                  height={100}
-                />
-              </a>
-              <a href="#">
-                <Image
-                  src="/images/download/leaflet.jpg"
-                  alt="leaflet"
-                  width={235}
-                  height={100}
-                />
-              </a>
-              <a href="#">
-                <Image
-                  src="/images/download/srtpernyataan.jpg"
-                  alt="suratpernyataan"
-                  width={235}
-                  height={100}
-                />
-              </a>
-              <a href="#">
-                <Image
-                  src="/images/download/jadwal.jpg"
-                  alt="jadwal"
-                  width={235}
-                  height={100}
-                />
-              </a>
-              <a href="#">
-                <Image
-                  src="/images/download/skmaba.jpg"
-                  alt="skmaba"
-                  width={235}
-                  height={100}
-                />
-              </a>
+              <a href="/file-download/statutauntirta.pdf" className="w-[260px] border text-center rounded-full bg-gradient-to-tr hover:bg-gradient-to-bl duration-200 text-white from-gray-900 to-amber-950 p-1 flex" download={"statutauntirta.pdf"}><div className="w-[70%]"><span className="font-extrabold">Download</span> <br/> <span className="text-xs">Statuta Untirta</span></div><FaDownload className="self-center w-[30%]" size={25}/></a>
+              <a href="/file-download/skpsppi.pdf" className="w-[260px] border text-center rounded-full bg-gradient-to-tr hover:bg-gradient-to-bl duration-200 text-white from-gray-900 to-amber-950 p-1 flex" download={"skpsppi.pdf"}><div className="w-[70%]"><span className="font-extrabold">Download</span> <br/> <span className="text-xs">SK Pendirian PSPPI Untirta</span></div><FaDownload className="self-center w-[30%]" size={25}/></a>
+              <a href="/file-download/sertifakreditasi.pdf" className="w-[260px] border text-center rounded-full bg-gradient-to-tr hover:bg-gradient-to-bl duration-200 text-white from-gray-900 to-amber-950 p-1 flex" download={"sertifakreditasi.pdf"}><div className="w-[70%]"><span className="font-extrabold">Download</span> <br/> <span className="text-xs">Sertifikat Akreditasi</span></div><FaDownload className="self-center w-[30%]" size={25}/></a>
+              <a href="/file-download/formcv.pdf" className="w-[260px] border text-center rounded-full bg-gradient-to-tr hover:bg-gradient-to-bl duration-200 text-white from-blue-950 to-amber-950 p-1 flex" download={"formcv.pdf"}><div className="w-[70%]"><span className="font-extrabold">Download</span> <br/> <span className="text-xs">Format curriculum vitae</span></div><FaDownload className="self-center w-[30%]" size={25}/></a>
+              <a href="/file-download/pernyataankeaslian.pdf" className="w-[260px] border text-center rounded-full bg-gradient-to-tr hover:bg-gradient-to-bl duration-200 text-white from-blue-950 to-amber-950 p-1 flex" download={"pernyataankeaslian.pdf"}><div className="w-[70%]"><span className="font-extrabold">Download</span> <br/> <span className="text-xs">Surat pernyataan keaslian dokumen</span></div><FaDownload className="self-center w-[30%]" size={25}/></a>
+              <a href="/file-download/pernyataankesanggupan.pdf" className="w-[260px] border text-center rounded-full bg-gradient-to-tr hover:bg-gradient-to-bl duration-200 text-white from-blue-950 to-amber-950 p-1 flex" download={"pernyataankesanggupan.pdf"}><div className="w-[70%]"><span className="font-extrabold">Download</span> <br/> <span className="text-xs">Surat pernyataan kesangupan pembiayaan</span></div><FaDownload className="self-center w-[30%]" size={25}/></a>
             </div>
           </section>
           <Footer />
